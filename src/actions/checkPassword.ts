1805208b478282597499b8db1191c05e9eca78fc
Rich Cahill <rich@rc3.me>
2025-04-13 15:27:19 +0100

    Update passwords

--- src/actions/checkPassword.ts
+++ src/actions/checkPassword.ts
@@ -1,6 +1,6 @@
 "use server";
 
-const passwords = ["icp24", "ICP24", "polene"];
+const passwords = ["icp24", "ICP24", "icp", "ICP", "icp25", "ICP25"];
 
 export async function checkPassword(code: string) {
   console.log("Checking password", code);
